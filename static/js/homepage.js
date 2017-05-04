c76bbc7133f8a7be760c986cc3e700686d3e2293
Alif Faishol <alifaishol@gmail.com>
2017-05-04 20:32:41 +0700

    update portfolio carousel

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
     $('.portfolio-slides').slick({
-        slidesToShow: 2,
-        adaptiveHeight: true
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.portfolio-slides-nav'
+    });
+    $('.portfolio-slides-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.portfolio-slides',
+        centerMode: true,
+        focusOnSelect: true
     });
 });
